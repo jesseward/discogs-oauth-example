68d323f826bf25c029e8141e048d2c3f93e35205
Jesse Ward <jesse.ward@gmail.com>
2020-08-03 08:32:05 -0400

    Migrating discogs_client_example to python3 and new lib.
    The official Discogs Client lib was deprecated in early June 2020. Example is now
    using the python3-discogs-client lib (https://github.com/joalla/discogs_client).

--- discogs_client_example.py
+++ discogs_client_example.py
@@ -32,59 +32,56 @@ discogsclient = discogs_client.Client(user_agent)
 discogsclient.set_consumer_key(consumer_key, consumer_secret)
 token, secret, url = discogsclient.get_authorize_url()
 
-print ' == Request Token == '
-print '    * oauth_token        = {0}'.format(token)
-print '    * oauth_token_secret = {0}'.format(secret)
-print
+print(' == Request Token == ')
+print(f'    * oauth_token        = {token}')
+print(f'    * oauth_token_secret = {secret}')
+print()
 
 # Prompt your user to "accept" the terms of your application. The application
 # will act on behalf of their discogs.com account.
 # If the user accepts, discogs displays a key to the user that is used for
 # verification. The key is required in the 2nd phase of authentication.
-print 'Please browse to the following URL {0}'.format(url)
+print(f'Please browse to the following URL {url}')
 
 accepted = 'n'
 while accepted.lower() == 'n':
     print
-    accepted = raw_input('Have you authorized me at {0} [y/n] :'.format(url))
+    accepted = input(f'Have you authorized me at {url} [y/n] :')
 
 
 # Waiting for user input. Here they must enter the verifier key that was
 # provided at the unqiue URL generated above.
-oauth_verifier = raw_input('Verification code :').decode('utf8')
+oauth_verifier = input('Verification code : ')
 
 try:
     access_token, access_secret = discogsclient.get_access_token(oauth_verifier)
 except HTTPError:
-    print 'Unable to authenticate.'
+    print('Unable to authenticate.')
     sys.exit(1)
 
 # fetch the identity object for the current logged in user.
 user = discogsclient.identity()
 
 print
-print ' == User =='
-print '    * username           = {0}'.format(user.username)
-print '    * name               = {0}'.format(user.name)
-print ' == Access Token =='
-print '    * oauth_token        = {0}'.format(access_token)
-print '    * oauth_token_secret = {0}'.format(access_secret)
-print ' Authentication complete. Future requests will be signed with the above tokens.'
+print(' == User ==')
+print(f'    * username           = {user.username}')
+print(f'    * name               = {user.name}')
+print(' == Access Token ==')
+print(f'    * oauth_token        = {access_token}')
+print(f'    * oauth_token_secret = {access_secret}')
+print(' Authentication complete. Future requests will be signed with the above tokens.')
 
 # With an active auth token, we're able to reuse the client object and request
 # additional discogs authenticated endpoints, such as database search.
-search_results = discogsclient.search('House For All', type='release',
-        artist='Blunted Dummies')
+search_results = discogsclient.search('House For All', type='release', artist='Blunted Dummies')
 
-print '\n== Search results for release_title=House For All =='
+print('\n== Search results for release_title=House For All ==')
 for release in search_results:
-    print '\n\t== discogs-id {id} =='.format(id=release.id)
-    print u'\tArtist\t: {artist}'.format(artist=', '.join(artist.name for artist
-                                         in release.artists))
-    print u'\tTitle\t: {title}'.format(title=release.title)
-    print u'\tYear\t: {year}'.format(year=release.year)
-    print u'\tLabels\t: {label}'.format(label=','.join(label.name for label in
-                                        release.labels))
+    print(f'\n\t== discogs-id {release.id} ==')
+    print(f'\tArtist\t: {", ".join(artist.name for artist in release.artists)}')
+    print(f'\tTitle\t: {release.title}')
+    print(f'\tYear\t: {release.year}')
+    print(f'\tLabels\t: {", ".join(label.name for label in release.labels)}')
 
 # You can reach into the Fetcher lib if you wish to used the wrapped requests
 # library to download an image. The following example demonstrates this.
@@ -92,9 +89,9 @@ image = search_results[0].images[0]['uri']
 content, resp = discogsclient._fetcher.fetch(None, 'GET', image,
                 headers={'User-agent': discogsclient.user_agent})
 
-print ' == API image request =='
-print '    * response status      = {0}'.format(resp)
-print '    * saving image to disk = {0}'.format(image.split('/')[-1])
+print(' == API image request ==')
+print(f'    * response status      = {resp}')
+print(f'    * saving image to disk = {image.split("/")[-1]}')
 
-with open(image.split('/')[-1], 'w') as fh:
+with open(image.split('/')[-1], 'wb') as fh:
     fh.write(content)
